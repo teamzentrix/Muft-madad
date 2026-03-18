44f5c301f5b46943879926298591d72dbd8d8cae
teamzentrix <team.zentrixinfotech@gmail.com>
2026-03-18 15:15:34 +0530

    ok

--- backend/routes/doctors.routes.js
+++ backend/routes/doctors.routes.js
@@ -4,7 +4,7 @@ const doctorsController = require('../controllers/doctors.controller');
 
 router.post('/', doctorsController.createDoctor);
 router.get('/', doctorsController.getAllDoctors);
-router.get('/id/:id', doctorsController.getDoctorById);
+router.get('/id/:uuid', doctorsController.getDoctorByUuid)
 router.get('/:uuid', doctorsController.getDoctorByUuid);
 router.put('/:uuid', doctorsController.updateDoctor);
 router.delete('/:uuid', doctorsController.deleteDoctor);
